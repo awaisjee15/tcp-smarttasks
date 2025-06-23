a03b6cee6147e68203e1bdc8566009d6f22ea917
HussainTechGee <hussaintechlab@gmail.com>
2025-06-23 17:42:24 +0500

    Initial project setup and floder structure

---
+++ SmartTasks/SmartTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartTasksApp.swift
+//  SmartTasks
+//
+//  Created by AwaisSalim on 23/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct SmartTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SmartTasksTests/SmartTasksTests.swift
@@ -0,0 +1,16 @@
+//
+//  SmartTasksTests.swift
+//  SmartTasksTests
+//
+//  Created by AwaisSalim on 23/06/2025.
+//
+
+import Testing
+
+struct SmartTasksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
